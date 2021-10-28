dcc55fc6293da801605da0b53ef2f48580fda3c6
Adeline <gcyanide@list.ru>
2021-10-28 15:20:25 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Automation;
+
+namespace MasterDataTestSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
